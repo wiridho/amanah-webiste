6ce742ad1ba1bfa3f97a3aee3f05210f5b69a3c4
wiridho <tambunanwiridho@gmail.com>
2023-06-16 04:19:30 +0700

    Fix Active Link Pathname

--- src/components/molekul/custom-link/CustomLink.js
+++ src/components/molekul/custom-link/CustomLink.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import ButtonIcon from "../button-icon/ButtonIcon";
 
 const CustomLink = ({ children, to, className, icon }) => {
+  const { pathname } = useLocation();
+  const isActive = pathname.includes(`/${to}`);
   return (
     <Link
       to={to}
-      className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${className}`}
+      className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${className} ${
+        isActive ? "bg-slate-100 font-semibold text-indigo-500" : ""
+      }`}
     >
-      <ButtonIcon icon={icon} className="text-sm tracking-wide truncate">
+      <ButtonIcon
+        icon={icon}
+        className={`text-sm tracking-wide truncate ${
+          isActive ? "bg-slate-100 font-semibold text-indigo-500" : ""
+        } `}
+      >
         {children}
       </ButtonIcon>
     </Link>
